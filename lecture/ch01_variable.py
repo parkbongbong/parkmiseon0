4cf523f71c87264e9d6c35614d53d458fe52a32a
parkbongbong <parkmiseon0125@naver.com>
2024-10-18 13:25:42 +0900

    Update ch01_variable.py

--- lecture/ch01_variable.py
+++ lecture/ch01_variable.py
@@ -53,8 +53,56 @@ print(float(d))
 # -> NullPointerException(크리티컬 오류 -> 프로그램 종료)
 # - 만약에 None 또는 Null  -> ""
 
-# 변수 생성 및 초기화
+# 5.변수 생성 및 초기화
 # - num = 5
-# Python은 변수 선언시 type을 지정하지 않음!(동적 타이핑 언어)
-# - =(대입 연산자) -> 우측의 값을 좌측에 대입
-# - 
+# - Python은 변수 선언시 type을 지장하지 않음(동적 타이핑 언어)
+# - =(대입연산자) -> 우측의 값을 좌측에 대입
+# - 초기화: 초기 변수를 생성하면 변수를  생성한 메모리 공간에
+#           쓰레기 파일들이 존재 ,변수에 값을 대입하면
+#            쓰레기 파일들이 삭제(초기화) 돼고 값만 저장 
+
+# 6. 명명규칙(naming rule)
+# - 변수,함수,클래스 등의 사용자 정의 이름에 사용
+# - 몀확하고 알아보기 쉽게
+#  1. 영어 대소문자,숫자,특수문자(_,-)만 사용
+#  2. 숫자로 시작할 수 없습니다
+#     123abc(x) abc123(o)
+#  3. 영어 대소문자 구별
+#      Abc!=abc!=ABC
+#  4. 예약어 사용불가
+#     *예약어: python에서 미리 선점하여 사용중인 키워드
+#      print,for, while,if,else,class,and,return
+#      import,def,pass ...
+
+# 7. 네이밍 메서드
+# - 변수,함수,클랫, 등의 사용자 정의 이름에 사용하는 기법
+# - 프로그래밍 언어별로 naming mehtod가 다름
+# 1)PascalCase (StudentNumber)
+# 2)camlCase   (studentNumber)
+# 3)snake_case  (student_number)
+# 4)kebab-case  (student-number)
+
+#          변수       함수        클래스
+# JAVA     카멜       카멜        파스칼
+# PYTHON   스네이크   스네이크     파스칼
+
+# 8. 상수
+# - 변하지 않는 수
+# - *상수는 반드시 선언 및 초기화(고정)를 함께
+# - 전부다 대문자 사용
+MAX_VALUE = 30 #상수
+
+# 9. 동적 출력
+# - print()-> 출력(+변수)
+student_num = 24
+name = "홍길동"
+printf("조선대학교 24학번 홍길동입니다") #하드 코딩(지양)
+#  1.format()-old
+printf("조선대학교 {}학번 {}입니다".format(student_num,name))
+#  2.f-string -new
+printf(f"조선대학교 {student_num}학번 {name}입니다")
+
+# 10. 간단한 사칙연산
+# - 5/2   -> 2.5
+# - 5//2  -> 몫(2)
+# - 5%2   -> 나머지(1)
