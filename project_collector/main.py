f56cd63402a66e974ebb5bcd8694bfcd0c42dcb9
parkbongbong <parkmiseon0125@naver.com>
2024-11-28 12:21:38 +0900

    Add files via upload

--- project_collector/main.py
+++ project_collector/main.py
@@ -16,6 +16,10 @@ import re  # 정규식
 
 # streamlit run project_collector/main.py
 
+# "Pandas"의 DataFrame -> CSV로 변경
+def convert_df(df):
+    return df.to_csv(index=False, encoding="utf8")
+
 category = "digital"
 def main():
     st.set_page_config(
@@ -25,16 +29,30 @@ def main():
     st.title("NEWS: :blue[Collector]")
     st.text("DAUM 뉴스를 수집합니다.")
     
-    with st.form(key="form"):
+   
         # 1. 정규식 -> 문자만 추출(숫자, 특수문자 제거!)
         # 2. 수집 데이터를 엑셀로 다운로드
         # 3. README.md 작성! -> 프로젝트 정리(Github)
         
         
         # 사용자가 수집버튼을 클릭하면 -> 클릭 이벤트
-        submitted = st.form_submit_button("수집")
-        if submitted:
-            collect_news()
+    flag = False
+    if st.button("수집"):
+        df_news, count = collect_news()
+        st.write(f"뉴스 {count}건 수집 완료")
+        st.write(df_news)
+        flag = True
+        news_csv = convert_df(df_news)
+        
+    # 수집 성공한 경우 엑셀 다운로드 버튼 활성화!
+    if flag:
+        st.download_button(
+            label="다운로드",
+            data=news_csv,
+            file_name=f"실시간뉴스.csv",
+            mime="text/csv",
+            key="download_csv"
+        )
            
     
 if __name__ == "__main__":
