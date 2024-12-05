d8d110fbd7d6f945e7231239e53c50582dfc6f4c
parkbongbong <parkmiseon0125@naver.com>
2024-12-05 12:29:18 +0900

    Add files via upload

---
+++ project_lms/main.py
@@ -0,0 +1,123 @@
+# 도서 관리 시스템
+# 1. 도서 기능 한정적으로 개발
+# 2. CRUD 개발 학습
+
+# 프로그래밍 언어 + Database
+# Create  : 데이터 저장
+# Read    : 데이터 조회
+# Update  : 데이터 수정
+# Delete  : 데이터 삭제
+
+# streamlit run project_lms/main.py
+
+import streamlit as st
+import pandas as pd
+from service import book_service
+
+##########################
+## 1. 프로그램 초기 설정 ##
+#########################
+st.set_page_config(
+    page_title="도서관리 시스템",
+    page_icon=""  # favicon
+    
+)
+
+# 1-1. Stramlit 세션 상태에서 현재 페이지를 추적하기 위한 초기 설정
+# session_state: 웹사이트의 현재 상태값을 저장
+# -> session_state값에 Page가 없으면
+#    기본적으로 main을 저장
+#    = Main 페이지 실행
+if "page" not in st.session_state:
+    st.session_state["page"] = "main"
+    
+# 1-2. 다른 페이지로 이동하는 함수
+def navigate_to(page):
+    st.session_state["page"]=page
+    st.rerun()
+    
+# 1-3. 기본 디자인 작업(CSS)
+st.markdown("""
+    <style>
+        .block-container {
+            padding-top: 2rem;
+            padding-left: 0rem !important;
+            padding-right: 0rem !important;
+            max-width: 55rem !important;
+        }
+        
+        hr {
+            margin: 0;
+            padding: 0;
+        }
+
+        .stButton>button {
+            width: 5rem;
+            padding: 0;
+            margin: 0;
+            text-align: center;
+            font-size: 16px;
+            cursor: pointer;
+        }
+
+        .stColumns {
+            gap: 0px; /* 열 간의 기본 간격을 없애기 */
+        }
+
+    </style>
+
+""", unsafe_allow_html=True)
+
+
+###############
+## 2. HEADER ##
+###############
+
+st.title("도서관리 시스템")
+st.markdown("<hr>",unsafe_allow_html=True)
+
+col1, col2 = st.columns(2)
+with col1:
+    if st.button("HOME"):
+        navigate_to("main")
+with col2:
+    if st.button("등록"):
+        navigate_to("insert")
+st.markdown("<hr>",unsafe_allow_html=True)
+
+#############
+## 3. BODY ##
+#############
+
+def main_page():  # 조회, 검색, 삭제
+     # [전체 데이터 조회]
+    rows = book_service.get_books()
+    
+    # [데이터 검색]
+    with st.form("search_form"):
+        keyword = st.text_input("도서검색")
+        submitted = st.form_submit_button("검색")
+        if submitted:
+            rows = book_service.search_books(keyword)
+            st.write(f'"{keyword}"로 검색된 결과는 총 {len(rows)}건 입니다.')
+            
+    # st.dataframe() -> Streamlit에서 표 형태로 출력
+    event = st.dataframe(rows,
+                         on_select="rerun",
+                         selection_mode="single-row",
+                         use_container_width=True,
+                         hide_index=True)
+    
+def insert_page():  # 등록
+    pass
+def update_page():  # 수정
+    pass
+
+if st.session_state["page"] == "main":
+    main_page()
+elif st.session_state["page"] == "insert":
+    insert_page()
+elif st.session_state["page"] == "update":
+    update_page()
+
+    
---
+++ project_lms/service/book_service.py
@@ -0,0 +1,103 @@
+# 프론트엔드 - Controller - Service - DAO - DB
+# Streamlit - main.py    - service       - DB
+
+# 서비스층 : 실제 비즈니스로직의 기능을 담당
+
+# MVC, MVT 패턴
+# -> 시스템 개발시 사용하는 패턴
+
+# 웹브라우저 요청 : http://naver.com/cafe + 글쓰기
+#      ↓
+# 웹서버(네이버 서버)
+#  1. Controller : 요청을 받아서 해당 로직을 처리할 수 있는  Service로 전달하는 역할
+#     -> Cafe 관련  Service로 전달
+#  2. Service : 실제 비지니스 로직을 처리하는 곳
+#     -> Cafe 글쓰기 관련 함수(def cafe_insert())로 처리
+#     -> def cafe_insert() 처리하는 도중에 DB가 필요한 경우 DAO로 전달
+#  3. DAO(Data Access Object) : DB관련 로직을 처리
+#     -> CafeDAO를 사용해서 SQL(INSERT INTO ~ ) 실행
+#     -> DB로부터 처리받은 결과를 Service로 전달
+#  4. Service
+#     -> DAO로부터 처리받은 결과를 Controller로 전달
+#  5. Controller
+#     -> Service로부터 처리받은 결과를 웹브라우저에게 전달(Response)
+
+# 겨울방학 추천 코스
+# 1. SQLD 자격증 취득
+# 2. 웹 개발 공부
+#   -> 웹 개발
+#   -> Python 웹 프레임워크: Django, Flask(비추), FastAPI
+#   ※ Python 웹 개발자 취업? 매우 어렵, 잘 안써(점유율 ↓)
+#       1등 : SpringBoot(JAVA)
+#       2등 : Node.js(JavaScript) 
+#       3등 : Django(Python)
+#      ※ HTML, CSS, JS(기본) -> 1달 공부
+
+from common.connection import connection
+import pandas as pd
+
+# 도서 목록 조회(ALL)
+def get_books():
+    conn = connection()
+    try:
+        curs = conn.cursor()
+        sql = """
+            SELECT * FROM tbl_book;
+        """
+        curs.execute(sql)  # 실행문
+        # 전체결과 -> DICT -> 2차원데이터
+        rows = curs.fetchall()  # 실행결과(전체) 받기
+        # 2차원 DICT 데이터 -> 표 형태로 변환
+        rows = pd.DataFrame(rows)
+        print(rows)
+        # Python의 DataFrame 형태로 변환
+    except Exception as e:
+        print(e)
+    finally:
+        if curs:
+            curs.close()
+        if conn and conn.open:
+            conn.close()
+            
+    return rows
+
+# 도서검색
+def search_books(keyword: str):
+    conn = connection()
+    
+    try:
+        curs = conn.cursor()
+        sql = """
+            SELECT *
+            FROM tbl_book
+            WHERE book_name LIKE %(keyword)s
+            OR book_writer LIKE %(keyword)s
+            OR book_publisher LIKE %(keyword)s;
+        """
+        curs.execute(sql, {"keyword" : "%"+keyword+"%"})
+        rows = curs.fetchall()
+        rows = pd.DataFrame(rows)
+        
+    except Exception as e:
+        print(e)
+        
+    finally:
+        if curs:
+            curs.close()
+        if conn and conn.open:
+            conn.close()
+
+    return rows
+        
+
+# 도서등록
+def insert_book(book: dict):
+    pass
+
+# 도서수정
+def update_book(book: dict):
+    pass
+
+# 도서삭제
+def delete_book(book_isbn: dict):
+    pass
